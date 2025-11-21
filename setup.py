a592a0ebf3a272d7e3fb11676135b9ae1991edfc
berke-volkan <berkevolkan0909878909@gmail.com>
2025-11-22 00:10:05 +0300

    .

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="snowflake-security",
-    version="0.1",
+    version="0.0.1",
     packages=find_packages(),
     install_requires=["dotenv","convex","cryptography","pyotp","prompt_toolkit"],
 )
